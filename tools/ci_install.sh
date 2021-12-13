c11403dff2a9ddb10110042e9839a1d3a438771e
Evgeny Posenitskiy <45995097+q-posev@users.noreply.github.com>
2021-12-13 11:24:08 +0100

    install TREXIO in ci_install.sh script

--- tools/ci_install.sh
+++ tools/ci_install.sh
@@ -4,6 +4,15 @@
 # the library dependencies and build it with Verificarlo with vfc_probes support
 # enabled.
 
+export VERSION=2.0
+wget https://github.com/TREX-CoE/trexio/releases/download/v${VERSION}/trexio-${VERSION}.0.tar.gz
+tar -zxf trexio-${VERSION}.0.tar.gz
+cd trexio-${VERSION}.0
+./configure --prefix=/usr --host=x86_64 CC="verificarlo-c" FC="verificarlo-f"
+make -j4
+sudo make install
+cd ..
+
 ./autogen.sh
 QMCKL_DEVEL=1 ./configure --prefix=$PWD/_install \
 --enable-silent-rules --enable-maintainer-mode --enable-vfc_ci --host=x86_64 \
